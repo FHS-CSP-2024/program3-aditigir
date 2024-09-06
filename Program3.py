116f9bfdc7e8a147637006f2265f2c30d7a20128
aditigir <aditigirish31@gmail.com>
2024-09-06 14:55:56 +0000

    Doneee!

--- Program3.py
+++ Program3.py
@@ -1,3 +1,4 @@
+
 ## Problem 1 ##
 #Fix the following code so that its output matches
 # Sample output:
@@ -22,7 +23,8 @@ level3 = "semiprofessional"
 lower = 2000
 upper = 3000
 
-print("my name is ", name, " , I am ", age, "years old")
+print("my name is ", name, " , I am ", 
+age, "years old")
 print("my skills are")
 print("- ", skill1, " (", level1, ")")
 print("- ", skill2, " (", level2, ")")
@@ -50,4 +52,7 @@ print("I am looking for a job with a salary of", lower, "-", upper, "dollars per
 
 x = input("X val: ")
 y = input("Y val: ")
-
+print(int(x) + int(y))
+print(int(x)-int(y))
+print(int(x)*int(y))
+print(int(x)/int(y))
